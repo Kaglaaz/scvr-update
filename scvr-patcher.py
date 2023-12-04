24e3e736f6555db2f22d2955f60870aaa01ae080
Bluscream <Bluscream@users.noreply.github.com>
2023-12-04 19:32:58 +0100

    Add directory picker dialog
    (if default dir isn't found)
    [Requires new config.json structure aswell]

--- scvr-patcher.py
+++ scvr-patcher.py
@@ -2,6 +2,7 @@ import os
 import json
 import tkinter as tk
 from tkinter import *
+import tkinter.filedialog
 from PIL import Image, ImageTk
 from io import BytesIO
 import base64
@@ -99,7 +100,14 @@ def update_data():
             break
 
     if star_citizen_dir is None:
-        messagebox.showerror("Error", "Star Citizen installation directory not found.")
+        messagebox.showerror("Error", "Star Citizen installation directory not found at default location.\nPlease select directory manually!")
+        star_citizen_dir = tkinter.filedialog.askdirectory(
+            initialdir="C:\Program Files",
+            title="Select Star Citizen installation directory (Containing LIVE, PTU, EPTU, TECH-PREVIEW, HOTFIX, ...)"
+        )
+
+    if not os.path.exists(star_citizen_dir):
+        messagebox.showerror("Error", "Star Citizen installation directory not found!")
         return
 
     # Extract width and height from the selected resolution
@@ -193,10 +201,25 @@ drives = [f"{letter}:" for letter in string.ascii_uppercase if os.path.exists(f"
 
 # Load headset data from the JSON file
 with open('configs.json', 'r') as json_file:
-    headset_data = json.load(json_file)
+    data = json.load(json_file)
+
+brands = data["hmds"]
 
 # Set default headset
+default_brand = "Select Manufacturer"
 default_headset = "Select VR Headset"
+default_config = "Select Configuration"
+
+# Label for brand selection
+headset_label = tk.Label(root, text="Select VR Headset brand in the dropdown below:", anchor="c", font=("Arial", 12))
+headset_label.pack(padx=10, pady=10, anchor="c")
+
+# Dropdown for brand selection
+brands_var = tk.StringVar()
+brands_keys = list(brands.keys())  # Updated to get the list of headsets dynamically
+brands_var.set(default_brand)
+headset_dropdown = tk.OptionMenu(root, brands_var, *brands_keys)
+headset_dropdown.pack(padx=10, pady=10, anchor="c")
 
 # Label for headset selection
 headset_label = tk.Label(root, text="Select VR Headset in the dropdown below \n(for PIMAX also choose lenses installed):", anchor="c", font=("Arial", 12))
@@ -204,10 +227,19 @@ headset_label.pack(padx=10, pady=10, anchor="c")
 
 # Dropdown for headset selection
 headset_var = tk.StringVar()
-headsets = list(headset_data.keys())  # Updated to get the list of headsets dynamically
 headset_var.set(default_headset)
-headset_dropdown = tk.OptionMenu(root, headset_var, *headsets)
+headset_dropdown = tk.OptionMenu(root, headset_var, "No headsets")
 headset_dropdown.pack(padx=10, pady=10, anchor="c")
+
+# Label for lens selection
+lense_label = tk.Label(root, text="Select Lense configuration in the dropdown below:", anchor="c", font=("Arial", 12))
+lense_label.pack(padx=10, pady=10, anchor="c")
+
+# Dropdown for lens selection
+lense_var = tk.StringVar()
+lense_var.set(default_config)
+lense_dropdown = tk.OptionMenu(root, lense_var, "No configurations")
+lense_dropdown.pack(padx=10, pady=10, anchor="c")
 
 # Label for resolution selection
 resolution_label = tk.Label(root, text="Select Resolution in dropdown below and then hit UPDATE:", anchor="c", font=("Arial", 12))
@@ -219,16 +251,47 @@ resolution_menu = tk.OptionMenu(root, resolution_var, "No Resolutions")
 resolution_menu.config(font=("Arial", 12))
 resolution_menu.pack(padx=10, pady=10, anchor="c")
 
+def update_brand_data(*args):
+    selected_brand = brands_var.get()
+    if not selected_brand: return
+    headset_dropdown['menu'].delete(0, 'end')  # Clear the existing menu
+    for hmd in brands[selected_brand].keys():
+        headset_dropdown['menu'].add_command(label=hmd, command=tk._setit(headset_var, hmd))
+    headset_var.set(list(brands[selected_brand].keys())[0])
+
+# Bind the update function to the brand dropdown
+brands_var.trace_add("write", lambda *args: update_brand_data(*args))
+
+def update_lens_data(*args):
+    selected_brand = brands_var.get()
+    if not selected_brand: return
+    selected_headset = headset_var.get()
+    if not selected_headset: return
+    lense_dropdown['menu'].delete(0, 'end')  # Clear the existing menu
+    keys = brands[selected_brand][selected_headset].keys()
+    for config in keys:
+        lense_dropdown['menu'].add_command(label=config, command=tk._setit(lense_var, config))
+    lense_var.set(list(brands[selected_brand][selected_headset].keys())[0])
+
+# Bind the update function to the brand dropdown
+headset_var.trace_add("write", lambda *args: update_lens_data(*args))
+
+def get_selections():
+    return brands_var.get(), headset_var.get(), lense_var.get(), resolution_var.get()
+
+def get_selected_data():
+    return brands[brands_var.get()][headset_var.get()][lense_var.get()]
+
 # Update FOV and resolutions based on the selected headset
 def update_headset_data(fov_var, *args):
-    selected_headset = headset_var.get()
+    selected_headset = get_selected_data()
     if selected_headset:
         # Fetch FOV and clamp it to a maximum of 120
-        fov_value = min(headset_data[selected_headset].get("SC Attributes FOV", 0), 120)
+        fov_value = min(selected_headset.get("SC Attributes FOV", 0), 120)
         fov_var.set(fov_value)
 
         # Fetch resolutions and update the dropdown
-        custom_resolutions = headset_data[selected_headset].get("Custom Resolution List", [])
+        custom_resolutions = selected_headset.get("Custom Resolution List", [])
         resolution_menu['menu'].delete(0, 'end')  # Clear the existing menu
 
         for resolution in custom_resolutions:
@@ -240,7 +303,7 @@ def update_headset_data(fov_var, *args):
             resolution_var.set("No Resolutions")
 
 # Bind the update function to the headset dropdown
-headset_var.trace_add("write", lambda *args: update_headset_data(fov_var, *args))
+lense_var.trace_add("write", lambda *args: update_headset_data(fov_var, *args))
 fov_var = tk.DoubleVar()
 
 # Button to update settings
